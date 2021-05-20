4c97f28b34ab7fe968936bbdbce621ba1e21aefa
Himanshu Seth <hseth9897@gmail.com>
2021-05-20 14:28:46 +0530

    updating our app with a shop page ,collection preview component and collectionitem component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/homepage/homepage.compnent";
-import "./pages/homepage.style.scss";
 
-const HatsPage = () => (
-  <div>
-    <h1>hats page</h1>
-  </div>
-);
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.compnent";
+
+import "./pages/homepage/homepage.style.scss";
+
+import ShopComponent from "./pages/shop/shop.component";
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopComponent} />
       </Switch>
     </div>
   );
---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './collection-item.style.scss'
+const CollectionItem =({id ,name,imageUrl ,price})=>(
+
+    <div className='collection-item' key={id}>
+        <div style={{backgroundImage:`url(${imageUrl})`}}
+        className="image"/>
+<div className='collection-fotter'>
+    <span className='name'>{name} </span>
+    <span className='price'>$ {price}</span>
+</div>
+    </div>
+)
+
+export default CollectionItem;
